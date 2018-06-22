df909441efd1b9650dea7487a24bcabb92ed32a6
LeeSangWon <ejsvk3284@gmail.com>
2018-06-22 22:27:47 +0900

    Revert "C++ 함수 정리"
    This reverts commit 3d0b18a13941a41384918e93f48ac7c97644ab5e.

--- RussianGame.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-int playerNumber(void);
-int bulletNumber(void);
-int setStartPlayer(int playNum);
-int playerAimed(int NowPlayer, int playNum);
-void delayThreeSec(void);
-short shotedPlayer(int bullet);
